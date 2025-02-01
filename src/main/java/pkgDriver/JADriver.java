28412ccee15d35ffcb4b5e05ca9130606b1366da
Jordan <jordanaldax@csus.edu>
2025-02-01 15:34:35 -0800

    Removed print statement from main, added printMessage function, added call to printMessage in main.

--- src/main/java/pkgDriver/JADriver.java
+++ src/main/java/pkgDriver/JADriver.java
@@ -2,6 +2,10 @@ package pkgDriver;
 
 public class JADriver {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        printMessage("Message from printMessage function");
     } // new comment
+
+    public static void printMessage(String my_string) {
+        System.out.println(my_string);
+    }
 }
