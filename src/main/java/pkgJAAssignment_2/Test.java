29acc0020f1903a0a2e33db93a3c64cccbb1f202
Jordan <jordanaldax@csus.edu>
2025-02-07 21:51:57 -0800

    [JATTTBoard.java]: Calls fillArray() when the object is created rather than when play() is called. Added isBoardFull() function. Searches through the entire 2D array. If any cell has '-', returns false. Else returns true. Added isCellFree() function. Returns true if the cell at the row and column is '-'. Else returns false. Added updateCell() function. Changes the character within the cell at the row and column to 'P'.

--- src/main/java/pkgJAAssignment_2/Test.java
+++ src/main/java/pkgJAAssignment_2/Test.java
@@ -4,10 +4,7 @@ public class Test {
     public static void main(String[] args) {
         pkgJATTTBackend.JATTTBoard my_board = new pkgJATTTBackend.JATTTBoard();
 
-        System.out.println("printBoard before fillArray:");
         my_board.printBoard();
         my_board.play();
-        System.out.println("printBoard after fillArray:");
-        my_board.printBoard();
     }
 }
--- src/main/java/pkgJATTTBackend/JATTTBoard.java
+++ src/main/java/pkgJATTTBackend/JATTTBoard.java
@@ -2,6 +2,11 @@ package pkgJATTTBackend;
 
 public class JATTTBoard {
     private char[][] board = new char[3][3];
+
+    // Fills the array with a '-' in each cell when the object is created.
+    public JATTTBoard() {
+        fillArray();
+    }
     private void fillArray() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -17,7 +22,23 @@ public class JATTTBoard {
             System.out.println();
         }
     }
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isCellFree(int row, int col) {
+        return board[row][col] == '-';
+    }
+    public void updateCell(int row, int col) {
+        board[row][col] = 'P';
+    }
     public void play() {
-        fillArray();
+        //
     }
 }
