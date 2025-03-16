c6c0ae31fdf8ae7e1b43084920cdf17cc989558b
Jordan <jordanaldax@csus.edu>
2025-03-16 14:26:30 -0700

    [JARenderer.java]: Altered variables FPV, VPT, and IPT to be final. Changed EPT to IPT. According to the assignment, we should have Vertices Per Tile (VPT), Floats Per Vertex (FPV), and Indices Per Tile (IPT). There was no EPT, and I'm unsure what EPT could have been a shorthand for, so I changed it to IPT suspecting they are intended to be the same thing. Added WIN_WIDTH, WIN_HEIGHT, WIN_POS_X, and WIN_POS_Y. This line has some variables that are used by code that I copied over. Altered OGL_MATRIX_SIZE to be final and set its value to 16. Copied code from Assignment 5 Driver to initOpenGL(). Copied code from Assignment 5 Driver to renderObjects(). I think I'll have to further change this code. Added renderLoop(). Copied code from Assignment5 Driver to renderLoop().

--- src/main/java/pkgJARenderEngine/JARenderer.java
+++ src/main/java/pkgJARenderEngine/JARenderer.java
@@ -1,9 +1,18 @@
 package pkgJARenderEngine;
 
 import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL;
 import pkgJAUtils.JAWindowManager;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwWaitEvents;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
 
 public class JARenderer {
     private int NUM_COLS;
@@ -11,10 +20,11 @@ public class JARenderer {
     private int SIZE;
     private int PADDING;
     private int OFFSET;
-    private int VPT;
-    private int EPT;
-    private int FPV;
-    private static int OGL_MATRIX_SIZE;
+    private final int FPV = 2; // Floats per Vertex
+    private final int VPT = 4; // Vertices per Tile
+    private final int IPT = 6; // Indices per Tile
+    static int WIN_WIDTH = 1800, WIN_HEIGHT = 1200, WIN_POS_X = 30, WIN_POX_Y = 90;
+    private static final int OGL_MATRIX_SIZE = 16;
 
     private int shader_program;
     private int[] winWidthHeight;
@@ -33,19 +43,81 @@ public class JARenderer {
         return null;
     }
 
-    private void initOpenGL() {
-        //
-    }
-
-    private void renderObjects() {
-        //
-    }
-
     private int[] generateTileIndices(final int rows, final int cols) {
         return null;
     }
 
+    private void initOpenGL() {
+        GL.createCapabilities();
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT);
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        this.shader_program = glCreateProgram();
+        int vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vs,
+                "uniform mat4 viewProjMatrix;" +
+                        "void main(void) {" +
+                        " gl_Position = viewProjMatrix * gl_Vertex;" +
+                        "}");
+        glCompileShader(vs);
+        glAttachShader(shader_program, vs);
+        int fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs,
+                "uniform vec3 color;" +
+                        "void main(void) {" +
+                        " gl_FragColor = vec4(0.6f, 0.7f, 0.1f, 1.0f);" +
+                        "}");
+        glCompileShader(fs);
+        glAttachShader(shader_program, fs);
+        glLinkProgram(shader_program);
+        glUseProgram(shader_program);
+        vpMatLocation = glGetUniformLocation(shader_program, "viewProjMatrix");
+        return;
+    }
+
+    private void renderObjects() {
+        while (!myWM.isGlfwWindowClosed()) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            int vbo = glGenBuffers();
+            int ibo = glGenBuffers();
+
+            float[] vertices = {-20f, -20f, 20f, -20f, 20f, 20f, -20f, 20f};
+            int[] indices = {0, 1, 2, 0, 2, 3};
+
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, (FloatBuffer) BufferUtils.
+                    createFloatBuffer(vertices.length).
+                    put(vertices).flip(), GL_STATIC_DRAW);
+            glEnableClientState(GL_VERTEX_ARRAY);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, (IntBuffer) BufferUtils.
+                    createIntBuffer(indices.length).
+                    put(indices).flip(), GL_STATIC_DRAW);
+            glVertexPointer(2, GL_FLOAT, 0, 0L);
+            viewProjMatrix.setOrtho(-100, 100, -100, 100, 0, 10);
+            glUniformMatrix4fv(vpMatLocation, false,
+                    viewProjMatrix.get(myFloatBuffer));
+            glUniform3f(renderColorLocation, 1.0f, 0.498f, 0.153f);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            final int VTD = 6; // need to process 6 Vertices To Draw 2 triangles
+            glDrawElements(GL_TRIANGLES, VTD, GL_UNSIGNED_INT, 0L);
+            myWM.swapBuffers();
+        }
+    }
+
     public void render(final int offset, final int padding, final int size, final int numRows, final int numCols) {
         //
+    }
+
+    private void renderLoop() {
+        glfwPollEvents();
+        initOpenGL();
+        renderObjects();
+        /* Process window messages in the main thread */
+        while (!myWM.isGlfwWindowClosed()) {
+            glfwWaitEvents();
+        }
     }
 }
