28326ed7953909c56eb24b944f05add61d45aa07
Jordan <jordanaldax@csus.edu>
2025-03-30 21:00:19 -0700

    [Driver.java]: Added file. Altered references to reflect the naming of my files.
    [JAPingPongArray.java]:
    Added file.
    Added empty methods with temporary parameters according to the calls in the driver file.

---
+++ src/main/java/pkgJAAssignment_7/Driver.java
@@ -0,0 +1,48 @@
+package pkgJAAssignment_7;
+
+import pkgJAUtils.JAPingPongArray;
+
+public class Driver {
+    public static void main(String[] my_args) {
+        final int ROWS = 7, COLS = 7;
+        int myMin = 10, myMax = 20;
+        JAPingPongArray myBoard = new JAPingPongArray(ROWS, COLS, myMin, myMax);
+        myBoard.swapLiveAndNext();
+        System.out.println("[10, 20) Board:");
+        myBoard.printArray();
+
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                myBoard.setCell(row, col, col);
+            }  //  for(int col = 0; col < COLS; ++col)
+        }  //  for(int row = 0; row < ROWS; ++row)
+        myBoard.swapLiveAndNext();
+        System.out.println("\n[0, COLS) Board:");
+        myBoard.printArray();
+
+        myBoard.randomizeViaFisherYatesKnuth();
+        myBoard.swapLiveAndNext();
+        System.out.println("\n[0, COLS) Board Randomized via FYK algorithm:");
+        myBoard.printArray();
+
+        myBoard.loadFile("neighbors_test.txt");
+        System.out.println("\n[0, 1] data file array:");
+        myBoard.swapLiveAndNext();
+        myBoard.printArray();
+
+        myBoard.updateToNearestNNSum();
+        myBoard.swapLiveAndNext();
+        System.out.println("\nNearest Neighbor sum array:");
+        myBoard.printArray();
+        myBoard.save("test_sum.txt");
+
+    }  //  public static void main(String[] my_args)
+}  //  public class Driver
+
+
+
+
+
+
+
+
---
+++ src/main/java/pkgJAUtils/JAPingPongArray.java
@@ -0,0 +1,45 @@
+package pkgJAUtils;
+
+public class JAPingPongArray {
+
+    int ROWS;
+    int COLS;
+    int myMin;
+    int myMax;
+
+    public JAPingPongArray(int rows, int cols, int min, int max) {
+        ROWS = rows;
+        COLS = cols;
+        myMin = min;
+        myMax = max;
+    }
+
+    public void swapLiveAndNext() {
+        //
+    }
+
+    public void printArray() {
+        //
+    }
+
+    public void setCell(int row, int col, int col2) {
+        //
+    }
+
+    public void randomizeViaFisherYatesKnuth() {
+        //
+    }
+
+    public void loadFile(String filename) {
+        //
+    }
+
+    public void updateToNearestNNSum() {
+        //
+    }
+
+    public void save(String filename) {
+        //
+    }
+
+}
