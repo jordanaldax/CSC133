42bdce9173d369bd8eaf0b79f14f8cac3e75ccb1
Jordan <jordanaldax@csus.edu>
2025-04-12 16:37:11 -0700

    [DriverTest_1.java]: Altered code to utilize the return value of the ULT to determine if the ULT passed or failed.
    [DriverTest_1.xml]:
    File added when creating the application configuration.

--- src/test/java/pkgJAAssignment_7/DriverTest_1.java
+++ src/test/java/pkgJAAssignment_7/DriverTest_1.java
@@ -5,5 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class DriverTest_1 {
     public static void main(String[] my_args) {
         System.out.println("Running DriverTest_1");
+        testRun();
+    }
+
+    private static void testRun() {
+        System.out.println("ult_a: " + passOrFail(ult_a()));
+    }
+
+    private static String passOrFail(boolean retVal) {
+        if(retVal)
+            return "PASS";
+        else
+            return "FAIL";
+    }
+
+    private static boolean ult_a() {
+        boolean retVal = false;
+
+        retVal = true;
+
+        return retVal;
     }
 }
