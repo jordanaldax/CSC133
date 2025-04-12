57bc9f55c3ff6376ebe7403ae0479264956a4e51
Jordan <jordanaldax@csus.edu>
2025-04-12 16:24:45 -0700

    [DriverTest_1.java]: Added file. This file is for the purposes of getting familiar with creating a test file.
    [ult_a_input.txt]:
    Added file. File provided by the professor.
    
    [ult_a_verify.txt]:
    Added file. File provided by the professor.

---
+++ src/test/java/pkgJAAssignment_7/DriverTest_1.java
@@ -0,0 +1,9 @@
+package pkgJAAssignment_7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverTest_1 {
+    public static void main(String[] my_args) {
+        System.out.println("Running DriverTest_1");
+    }
+}
